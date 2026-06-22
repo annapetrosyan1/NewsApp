12910754f0ac77c4b50a3d16369aa6870d8a7abb
Anna <80515880+annapetrosyan1@users.noreply.github.com>
2026-06-22 22:31:10 +0500

    Update NewsAppApp.swift

--- NewsApp/NewsAppApp.swift
+++ NewsApp/NewsAppApp.swift
@@ -11,22 +11,4 @@ struct NewsAppApp: App {
             NewsListView()
         }
     }
-    
-    private func setupTempAPIKey() {
-        #if DEBUG
-        let tempKey = "908229715ee245d7a5b4e3088b37b771"
-        
-        // проверяем, что ключ не пустой и сохраняем
-        if !tempKey.isEmpty {
-            let success = APIKeyManager.shared.saveAPIKey(tempKey)
-            if success {
-                print("✅ API ключ сохранен в Keychain")
-            } else {
-                print("❌ Ошибка сохранения ключа в Keychain")
-            }
-        } else {
-            print("❌ API ключ не найден. Вставьте ключ в setupTempAPIKey()")
-        }
-        #endif
-    }
 }
